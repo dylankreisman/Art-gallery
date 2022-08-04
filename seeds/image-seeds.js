683b0a2bf196e0848bd92db058cc48fada23f167
Mary Dillon <mary.dillon09@gmail.com>
2022-08-04 13:56:44 -0700

    gets images to show

--- seeds/image-seeds.js
+++ seeds/image-seeds.js
@@ -2,21 +2,21 @@ const { Image } = require('../models');
 
 const imageData = [
     {
-        hosted_url: "./seeds/images/20210104_102447.jpg",
+        hosted_url: "20210104_102447.jpg",
         category_id: "1",
         user_id: "1",
         image_name: "Beauty Within",
         description: "First sketched head, body then arm shape, then worked on skelelton from top down.",
     },
     {
-        hosted_url: "./seeds/images/20210117_122454.jpg",
+        hosted_url: "20210117_122454.jpg",
         category_id: "1",
         user_id: "2",
         image_name: "The Loop",
         description: "The endless chase of looking, blinding yourself of sight.",
     },
     {
-        hosted_url: "./seeds/images/20220804_140547.jpg",
+        hosted_url: "20220804_140547.jpg",
         category_id: "3",
         user_id: "3",
         image_name: "Fungi-Forest",
