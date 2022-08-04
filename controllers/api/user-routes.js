57e7ef3853f1601cbf5d6c02125862dbc0dda8d9
Dr Dyl <dylkreisman@gmail.com>
2022-08-04 08:06:32 -1000

    get all and one user

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,39 @@
+const router = require('exress').Router()
+const { User, Image, Category } = require('../../models')
+
+
+router.get('/',  (req, res) => {
+    User.findAll({
+        attributes: ['id', 'username','email','image_id','user_id'],
+        include: [
+        {
+            model: Image,
+            attributes: ['id', 'image_name', 'description', 'category_id']
+        }]
+    })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+    console.log(err)
+    res.status(500).json(err)
+})
+})
+
+router.get('/', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'username','email','image_id','user_id'],
+        include: [{
+            model: Image,
+            attributes: ["image_name"]
+        }]
+    })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+
