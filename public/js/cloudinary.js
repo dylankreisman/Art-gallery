cf77e1c63af89b63788c6e1766765bb9afea5d64
Mary Dillon <mary.dillon09@gmail.com>
2022-08-05 12:42:44 -0700

    adds cloudinary upload widget

---
+++ public/js/cloudinary.js
@@ -0,0 +1,11 @@
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dkwiaib2k', 
+    uploadPreset: 'my_preset'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+      }
+    }
+  )
+  document.getElementById("upload-widget").addEventListener("click", function(){
+      myWidget.open();
+    }, false);
