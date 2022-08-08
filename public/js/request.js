c8d9bf4e50a84cca2b5e058e186c0593389a7844
Shmuel Hoffman <snyh212@gmail.com>
2022-08-07 20:20:39 -0400

    added functional routes for routes

--- public/js/request.js
+++ public/js/request.js
@@ -4,14 +4,16 @@ const requestFormHandler = async (event) => {
     const categoryEl = document.querySelector('#request-category')
     const descriptionEl = document.querySelector('#request-description')
     const priceEl = document.querySelector('#request-price')
+    const usernameEl = document.querySelector('#request-username')
 
-    if (categoryEl && descriptionEl && priceEl) {
+    if (categoryEl && descriptionEl && priceEl && usernameEl) {
         const response = await fetch('/api/requests', {
             method: 'POST',
             body: JSON.stringify({ 
-                category: categoryEl.value, 
+                category_id: categoryEl.value, 
                 description: descriptionEl.value, 
-                price: priceEl.value
+                price: priceEl.value,
+                username: usernameEl.value
              }),
             headers: {'Content-Type': 'application/json'}
         
